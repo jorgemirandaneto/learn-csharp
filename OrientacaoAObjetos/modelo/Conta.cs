fb6f5ba33e16ed762a1ef806d7f0d85dadafa779
Jorge Miranda <jorge.miranda@sam.fortaleza.ce.gov.br>
2017-05-11 11:35:39 -0300

    Atualização

--- OrientacaoAObjetos/modelo/Conta.cs
+++ OrientacaoAObjetos/modelo/Conta.cs
@@ -4,4 +4,24 @@
     public double saldo;
     public double limite;
     public Agencia agencia;
+    
+    public void Deposita(double valor)
+    {
+        this.saldo += valor;
+    }
+
+    public void Saca(double valor)
+    {
+        this.saldo -= valor;
+    }
+
+    public void ImprimeExtrato()
+    {
+        System.Console.WriteLine("Seu saldo é: " + this.saldo);
+    }
+    public double SaldoDisponivel()
+    {
+        return this.saldo + this.limite;
+    }
+
 }
---
+++ OrientacaoAObjetos/testes/TestaMetodosConta.cs
@@ -0,0 +1,22 @@
+﻿class TestaMetodosConta
+{
+    static void Main(string[] args)
+    {
+        Conta c = new Conta();
+
+        System.Console.WriteLine("Chamada do metodo Deposita");
+        c.Deposita(1000);
+        c.ImprimeExtrato();
+        System.Console.WriteLine("-------------------------------------------------");
+
+        System.Console.WriteLine("Chamada do metodo Saca");
+        c.Saca(100);
+        c.ImprimeExtrato();
+        System.Console.WriteLine("-------------------------------------------------");
+
+        double saldoDisponivel = c.SaldoDisponivel();
+        System.Console.WriteLine("Seu saldo disponivel é: " + saldoDisponivel);
+
+
+    }
+}
